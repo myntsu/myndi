0a0b8ac91866c60f1cceb05ce6776e91ad4fe3a5
Myntsu <dingodille16@hotmail.com>
2023-05-18 13:10:38 -0400

    Added dynamic filtering to the blog posts, and added a limit of 2 blog posts in the main page

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -2,15 +2,54 @@ export function slugify(text) {
   return text
     .toString()
     .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]+/g, '')
-    .replace(/--+/g, '-')
-    .replace(/^-+/, '')
-    .replace(/-+$/, '');
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
 }
 
 export function formatDate(date) {
-    return new Date(date).toLocaleDateString('es-EN', {
-      timeZone: "UTC",
-    })
+  return new Date(date).toLocaleDateString("es-EN", {
+    timeZone: "UTC",
+  });
+}
+
+export function formatBlogPosts(
+  post,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = number,
+  } = {}
+) {
+  const filteredPosts = post.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    // filterOutDrafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  // sortByDate or randomize
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
   }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  return filteredPosts;
+}
