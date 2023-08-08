e62ea4038b942dc2763ab567477920bd6d1f9aaf
Myntsu <dingodille16@hotmail.com>
2023-08-08 01:00:27 -0400

    Worked on the about-me section: technologies

--- config.js
+++ config.js
@@ -13,5 +13,15 @@ export default {
       { platform: "youtube", link: "https://www.youtube.com/", handle: "@User" },
       { platform: "github", link: "https://github.com/", handle: "@User" },
     ],
+
+    stack: [
+        { language: "language-html5", name: "HTML", percentage: 70, color: "orange" },
+        { language: "language-css3", name: "CSS", percentage: 85, color: "blue" },
+        { language: "language-javascript", name: "JavaScript", percentage: 60, color: "yellow" },
+        { language: "rocket-launch", name: "Astro", percentage: 40, color: "orange" },
+        { language: "ruby", name: "Ruby", percentage: 15, color: "red" },
+        { language: "language-ruby-on-rails", name: "Ruby on Rails", percentage: 15, color: "red" },
+        { language: "database", name: "SQL", percentage: 35, color: "green" },
+      ],
   };
   
