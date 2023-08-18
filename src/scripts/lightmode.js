a27f99b5041914c08b95e96c369b904fcc12f387
Myntsu <dingodille16@hotmail.com>
2023-08-18 16:55:34 -0400

    Updating to Astro's latest version

---
+++ src/scripts/lightmode.js
@@ -0,0 +1,24 @@
+function applyPreferredTheme() {
+  if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
+
+applyPreferredTheme();
+// document.addEventListener("astro:beforeload", wr);
+
+function toggleTheme() {
+  if (localStorage.theme === "dark") {
+    localStorage.theme = "light";
+    document.documentElement.classList.add("light");
+    document.documentElement.classList.remove("dark");
+  } else {
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("light");
+  }
+}
+
+document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
