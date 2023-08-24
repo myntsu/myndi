ac1c573d91445bf0b231e0def23efee1a328cc82
Myntsu <dingodille16@hotmail.com>
2023-08-24 15:57:06 -0400

    Removing the spinner

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,18 +1,13 @@
 const imgWrappers = document.querySelectorAll('.img-wrapper');
 const dialog = document.querySelector('.pop-up-container');
 const expandedImg = dialog.querySelector('.expanded-image');
-const loadingSpinner = document.querySelector('.loading-spinner');
 
 imgWrappers.forEach(wrapper => {
   wrapper.addEventListener('click', () => {
     const img = wrapper.querySelector('img');
     const src = img.getAttribute('data-original-src');
     expandedImg.setAttribute('src', src);
-    loadingSpinner.style.display = 'block';
-    expandedImg.onload = () => {
-      loadingSpinner.style.display = 'none';
-      dialog.showModal();
-    };
+    dialog.showModal();
   });
 });
 
