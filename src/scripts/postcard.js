d496b9f8f4a311290b67f6d17ec83f0c7563177d
Myntsu <dingodille16@hotmail.com>
2023-08-25 17:22:16 -0400

    Changed the behavior of the navbar

--- src/scripts/postcard.js
+++ src/scripts/postcard.js
@@ -1,25 +1,14 @@
 
-// adding titles to headers and description
+// adding titles to headers
 const h2Elements = document.querySelectorAll('h2');
 h2Elements.forEach((h2Element) => {
   const h2Text = h2Element.innerText;
   h2Element.setAttribute('title', h2Text);
 });
 
+// adding titles to descriptions
 const descriptionElements = document.querySelectorAll('.description');
 descriptionElements.forEach((descriptionElement) => {
   const descriptionText = descriptionElement.innerText;
   descriptionElement.setAttribute('title', descriptionText);
 });
-
-// cards highlight on mouseover
-const updateCursor = ({ x, y }) => {
-  if (window.innerWidth >= 870) {
-    document.documentElement.style.setProperty("--x", x);
-    document.documentElement.style.setProperty("--y", y);
-  }
-};
-
-document.body.addEventListener("pointermove", updateCursor);
-
-
