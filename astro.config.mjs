4120fbe78971f8aebf57b05884a1d84f2ea7d8e8
Myntsu <dingodille16@hotmail.com>
2023-09-04 23:39:19 -0300

    Migrating to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   // sitemap
   site: "https://myndi.dev",
 
+  image: {
+    service: squooshImageService(),
+  },
+
   integrations: [
     // sitemap
     sitemap(),
-    // image
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
   ],
 
   markdown: {
